8f6651c2daea3dd6bd91757bbbc0cdef039b6c2f
kcchaitanya <connecttokc@gmail.com>
2021-08-04 17:02:48 +0530

    First Commit

---
+++ src/main/java/com/example/demoapp/HelloController.java
@@ -0,0 +1,15 @@
+package com.example.demoapp;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/helloWorld")
+    public String printHelloWorld(){
+        return "Hello World";
+    }
+
+}
