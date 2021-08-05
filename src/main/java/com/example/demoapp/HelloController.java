0fa7abb49feeddfc0e5a84b19540dc48e882cc01
kcchaitanya <connecttokc@gmail.com>
2021-08-05 10:11:45 +0530

    Updated Message

--- src/main/java/com/example/demoapp/HelloController.java
+++ src/main/java/com/example/demoapp/HelloController.java
@@ -9,7 +9,7 @@ public class HelloController {
 
     @GetMapping("/helloWorld")
     public String printHelloWorld(){
-        return "Hello World Welcome";
+        return "Hello World Welcome here";
     }
 
 }
